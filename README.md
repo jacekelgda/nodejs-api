90c7053842651e52f22d62a89a56931a448514ce
jacekelgda <jacekelgda@gmail.com>
2018-04-03 18:58:04 +0200

    Babel setup

---
+++ README.md
@@ -0,0 +1,13 @@
+### Requirements
+
+You will need npm and node installed on your host machine.
+
+### Installation
+
+`yarn`
+
+### Running local development
+
+`yarn dev`
+
+Api is accessible under port `3000` by default.
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import bodyParser from 'body-parser'
+import express from 'express'
+
+const port = process.env.PORT || 3000
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.listen(port)
